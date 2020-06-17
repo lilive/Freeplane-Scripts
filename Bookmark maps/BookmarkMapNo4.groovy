a5ec66bdd5d669f0e2b9bbb2ab8696dcbf586acd
lilive <lilivounet@gmail.com>
2020-06-17 15:27:10 +0200

    feat: Add bookmarking for whole maps

---
+++ Bookmark maps/BookmarkMapNo4.groovy
@@ -0,0 +1,15 @@
+// @ExecutionModes({on_single_node="/main_menu/navigate/Bookmarks"})
+
+// Use this script to bookmark the currently opened map
+// author: lilive
+
+// The bookmark identifier for this map
+id = "4"
+
+paths = [:]
+storagePath = c.getUserDirectory().toString() + File.separator + 'lilive_bookmarked_maps.json'
+file = new File( storagePath )
+if( file.exists() ) paths = new groovy.json.JsonSlurper().parseText( file.text )
+paths[ id ] = node.map.file.absolutePath
+json = groovy.json.JsonOutput.toJson( paths )
+file.write( json )
---
+++ Bookmark maps/JumpToMapNo3.groovy
@@ -0,0 +1,17 @@
+// @ExecutionModes({on_single_node="/main_menu/navigate/Bookmarks"})
+
+// Use this script to load a bookmarked map
+// author: lilive
+
+// The bookmark identifier for this map
+id = "3"
+
+storagePath = c.getUserDirectory().toString() + File.separator + 'lilive_bookmarked_maps.json'
+file = new File( storagePath )
+if( ! file.exists() ) return
+
+paths = new groovy.json.JsonSlurper().parseText( file.text )
+path = paths?[ id ]
+if( ! path ) return
+c.mapLoader( path ).withView().getMindMap()
+
