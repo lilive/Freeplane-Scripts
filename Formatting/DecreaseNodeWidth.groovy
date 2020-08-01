6996ba8b5e378f5eae35bc25e0f7d4cb9fb8545a
lilive <lilivounet@gmail.com>
2020-08-01 12:48:39 +0200

    feat: add scripts to control node width

---
+++ Formatting/DecreaseNodeWidth.groovy
@@ -0,0 +1,29 @@
+// @ExecutionModes({on_single_node="/menu_bar/format/menu_coreFormat/NodeWidth"})
+
+/*
+Decrease the minimal node width of each selected node by a "step" amount.
+If the minimal width of a node go above 0, unset it (equivalent to uncheck
+the minimal width checkbox in the format panel).
+Ensure that the new minimal width is a multiple of the step.
+
+Based on zipizap script for same purpose.
+*/
+
+// Decrease the width by this amount (you can change it)
+int step = 20
+
+// Get all selected nodes
+def nodes = c.getSelecteds()
+
+// Change nodes minimum width
+nodes.each{
+
+    // Discard node that do not have a minimum width
+    if( it.style.minNodeWidth < 0 ) return
+    
+    // Compute the new width to apply
+    int newWidth = (int)( it.style.minNodeWidth / step ) * step - step
+    if( newWidth <= 0 ) newWidth = -1
+    
+    it.style.minNodeWidth = newWidth
+}
---
+++ Formatting/IncreaseNodeWidth.groovy
@@ -0,0 +1,36 @@
+// @ExecutionModes({on_single_node="/menu_bar/format/menu_coreFormat/NodeWidth"})
+
+/*
+Increase the minimal node width of each selected node by a "step" amount.
+If more than one node is selected, the minimum with for each node is set
+to the minimum width of the larger one (after increment).
+Ensure that the new minimal width is a multiple of the step.
+
+Based on zipizap script for same purpose,
+and nnako "toggle block view for all siblings" script.
+*/
+
+import org.freeplane.view.swing.map.NodeView
+
+// Increase the width by this amount (you can change it)
+int step = 20
+
+// Get all selected nodes
+def nodes = c.getSelecteds()
+
+// Find the width of the larger node
+float maxDisplayWidth = -1
+nodes.each{
+    nodeView = it.delegate.viewers.find() {it instanceof NodeView}
+    width = nodeView?.mainView?.width
+    if( width > maxDisplayWidth ) maxDisplayWidth = width
+}
+
+// Compute the new width to apply
+int newWidth = maxDisplayWidth / c.zoom
+newWidth = (int)( newWidth / step ) * step + step
+
+// Change all nodes minimum width
+nodes.each{
+    it.style.minNodeWidth = newWidth
+}
